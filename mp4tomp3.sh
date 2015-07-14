6c2e591de0d466fe094bc749836c9d2ab269992e
felipesant0s <felipesant0stux@gmail.com>
2015-07-14 17:00:47 -0300

    Convert MP4 to MP3

--- mp4tomp3.sh
+++ mp4tomp3.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo -n "Qual arquivo deseja converter?: "
-read arq
-pacpl --to mp3 -v -r -bitrate 320 $arq
+
+pacpli=($(/usr/bin/aptitude search pacpl | awk '{print $1}'))
+  if [ "$pacpli" == "i" ]; then
+	echo -n "Qual arquivo deseja converter?: "
+	read arq
+	pacpl --to mp3 -v -r -bitrate 320 $arq
+  else
+  	echo -n "pacpl not installed, you want to install ? [y] or [n] "
+	read question
+	if [ "$question" == "y" ]; then
+	  sudo aptitude install pacpl
+	else
+	  exit 1
+	fi
+  fi
